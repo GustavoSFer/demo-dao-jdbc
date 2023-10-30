1c302a5f2bbde3ffaf71f8aa8f5e6d58cb173f5a
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-30 16:32:03 -0300

    Feat: Realizando o findById, buscando a informação de um seller no banco de dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,14 @@
 package application;
 
-import java.util.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Departament;
-import model.entities.Seller;
 
 public class Program {
 
   public static void main(String[] args) {
 
-    Departament depart = new Departament(1, "Eletronic");
-    Seller seller = new Seller(12, "João Silva", "joao@gmail.com", new Date(), 3500.00, depart);
-
-    System.out.println(seller);
-
     SellerDao sellerDao = DaoFactory.createSellerDao();
-
+    System.out.println(sellerDao.findById(3));
 
   }
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,10 +1,23 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Departament;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+
+  private Connection conn;
+
+  public SellerDaoJDBC(Connection conn) {
+    this.conn = conn;
+  }
 
   @Override
   public void insert(Seller obj) {
@@ -26,8 +39,39 @@ public class SellerDaoJDBC implements SellerDao {
 
   @Override
   public Seller findById(Integer id) {
-    // TODO Auto-generated method stub
-    return null;
+    PreparedStatement st = null;
+    ResultSet rs = null;
+    try {
+      st = conn.prepareStatement("SELECT seller.*, departament.nome as DepName " + "FROM seller "
+          + "INNER JOIN departament " + "ON seller.DepartamentId = departament.id "
+          + "WHERE seller.id = ?");
+
+      st.setInt(1, id);
+
+      rs = st.executeQuery();
+      // o retono da nossa busca sempre vem na posição 0 mas se estiver info. esta na posição 0
+      if (rs.next()) { // Se existir informação na proxima linha (1)
+        Departament departament = new Departament();
+        departament.setId(rs.getInt("departamentId"));
+        departament.setName(rs.getString("DepName"));
+
+        Seller seller = new Seller();
+        seller.setId(rs.getInt("id"));
+        seller.setName(rs.getString("nome"));
+        seller.setEmail(rs.getString("email"));
+        seller.setBirthDate(rs.getDate("birthDate"));
+        seller.setBaseSalary(rs.getDouble("baseSalary"));
+        seller.setDepartament(departament);
+
+        return seller;
+      }
+      return null;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeResultSet(rs);
+      DB.closeStatement(st);
+    }
   }
 
   @Override
